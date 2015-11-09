3b6b3592ce841bfc9f62b3a64555c4d5833440d0
krzys <krzys.kukla@gmail.com>
2015-11-10 00:37:04 +0100

    First commit

---
+++ src/main/java/pl/kukla/krzys/restaurant/App.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.restaurant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
